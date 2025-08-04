9e9d9733f0f8337290f2ce8b57c600570e5bb456
Liliia Galkina <vvlili@yandex.ru>
2025-08-04 16:29:54 +0300

    header auth без адаптива и логики

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import style from "./header.module.scss";
 import allstyle from "../allstyle.module.scss";
 import logo from "./img/logo.svg";
+import user from "./img/user.svg";
 
 export default function Header() {
     <div id="burger" className={style.burger}><span></span></div>
@@ -9,6 +10,7 @@ export default function Header() {
             <div className="container">
                 <div className={style.items}>
                     <div className={style.logo}><img src={logo} alt="логотип"/></div>
+                    <div className={style.navigation}>
                     <nav className={style.menu}>
                             <div id="burger" className={style.burger}><span></span></div>
                             <ul className={style.menulist}>
@@ -23,9 +25,21 @@ export default function Header() {
                                 </li>
                             </ul>
                     </nav>
+                    <div className={style.info}>
+                            <div className={style.companies}><p>Использовано компаний </p><span>34</span></div>
+                            <div className={style.limit}><p>Лимит по компаниям </p><span>100</span></div>
+                    </div>
+                    <div className={style.userinfo}>
+                            <div className={style.usertext}>
+                                <div className={style.username}>Алексей А. </div>
+                                <button className={style.logout}>Выйти</button>
+                            </div>
+                            <div className={style.image}><img src={user} alt="фото пользователя" /></div>
+                    </div>
                     <div className={style.authblock}>
                             <a href="#" className={style.authlink}>Зарегистрироваться</a>
                             <button className={`${style.authbutton} ${allstyle.button}`}>Войти</button>
+                    </div>
                     </div>
                 </div>
             </div>
