091ed1856ba73af1570999ebef1d47275f66891a
Liliia Galkina <vvlili@yandex.ru>
2025-08-12 16:02:50 +0300

    стилизация search с адаптивом

--- src/components/Main/MainPage/Search/Search.tsx
+++ src/components/Main/MainPage/Search/Search.tsx
@@ -3,8 +3,44 @@ import allstyles from "../../allstyle.module.scss"
 import mainimg from "./img/mainimg.svg";
 import documentlist from "./img/Document.svg";
 import folders from "./img/Folders.svg"; 
+import { useState } from "react";
+import check from "./img/check.svg"
 
 export default function Search() {
+  const [inn, setInn] = useState("");
+  const [ton, setTon] = useState("");
+  const [countDocs, setCountDocs] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [finishtDate, setFinishDate] = useState("");
+  const [maxFull, setMaxFull] = useState(false);
+  const [buisnessContext, setBuisnessContext] = useState(false);
+  const [mainRole, setMainRole] = useState(false);
+  const [onlyRisk, setOnlyRisk] =useState(false);
+  const [technicalNews, setTechnicalNews] = useState(false);
+  const [previews, setPreviews] = useState(false);
+  const [newsBulletin, setNewsBulletin] = useState(false);
+
+  const handleChangeInn = (e: React.ChangeEvent<HTMLInputElement>) =>{
+    setInn(e.target.value);
+  }
+
+   const handleChangeTon = (e: React.ChangeEvent<HTMLSelectElement>) => {
+     setTon(e.target.value);
+   };
+
+   const handleChangeCount = (e: React.ChangeEvent<HTMLInputElement>) => {
+     setCountDocs(e.target.value);
+   };
+
+    const handleChangeStartDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setStartDate(e.target.value);
+    };
+
+     const handleChangeFinishDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+       setFinishDate(e.target.value);
+     };
+
+
     return (
       <main className={style.main}>
         <div className="container">
@@ -30,8 +66,11 @@ export default function Search() {
                       type="text"
                       name="inn"
                       id="inn"
+                      value={inn}
+                      onChange={handleChangeInn}
                       className={`${style.input} ${style.inputinfo}`}
                       placeholder="10 цифр"
+                      required
                     />
                     <label htmlFor="ton" className={style.label}>
                       Тональность
@@ -39,6 +78,8 @@ export default function Search() {
                     <select
                       name="ton"
                       id="ton"
+                      value={ton}
+                      onChange={handleChangeTon}
                       className={`${style.input} ${style.inputinfo}`}
                     >
                       <option>любая</option>
@@ -52,50 +93,121 @@ export default function Search() {
                       type="text"
                       name="countdoc"
                       id="countdoc"
+                      value={countDocs}
+                      onChange={handleChangeCount}
                       className={`${style.input} ${style.inputinfo}`}
                       placeholder="От 1 до 1000"
+                      required
                     />
                   </div>
                   <div className={style.checkboxes}>
                     <div className={style.checkboxitem}>
-                      <input type="checkbox" className={style.checkbox} />
-                      <p className={style.checkboxtext}>
+                      <input
+                        type="checkbox"
+                        id="maxfull"
+                        name="maxfull"
+                        className={style.checkbox}
+                        checked={maxFull}
+                        onChange={() => setMaxFull(!maxFull)}
+                        style={{
+                          backgroundImage: maxFull ? `url(${check})` : "none",
+                        }}
+                      />
+                      <p
+                        className={style.checkboxtext}
+                        style={{ opacity: maxFull ? 1 : 0.4 }}
+                      >
                         Признак максимальной полноты
                       </p>
                     </div>
                     <div className={style.checkboxitem}>
-                      <input type="checkbox" className={style.checkbox} />
-                      <p className={style.checkboxtext}>
+                      <input
+                        type="checkbox"
+                        id="buisnesscontext"
+                        name="buisnesscontext"
+                        className={style.checkbox}
+                        checked={buisnessContext}
+                        onChange={() => setBuisnessContext(!buisnessContext)}
+                        style={{
+                          backgroundImage: buisnessContext
+                            ? `url(${check})`
+                            : "none",
+                        }}
+                      />
+                      <p
+                        className={style.checkboxtext}
+                        style={{ opacity: buisnessContext ? 1 : 0.4 }}
+                      >
                         Упоминания в бизнес-контексте
                       </p>
                     </div>
                     <div className={style.checkboxitem}>
-                      <input type="checkbox" className={style.checkbox} />
-                      <p className={style.checkboxtext}>
+                      <input
+                        type="checkbox"
+                        className={style.checkbox}
+                        checked={mainRole}
+                        onChange={() => setMainRole(!mainRole)}
+                      />
+                      <p
+                        className={style.checkboxtext}
+                        style={{ opacity: mainRole ? 1 : 0.4 }}
+                      >
                         Главная роль в публикации
                       </p>
                     </div>
                     <div className={style.checkboxitem}>
-                      <input type="checkbox" className={style.checkbox} />
-                      <p className={style.checkboxtext}>
+                      <input
+                        type="checkbox"
+                        className={style.checkbox}
+                        checked={onlyRisk}
+                        onChange={() => setOnlyRisk(!onlyRisk)}
+                      />
+                      <p
+                        className={style.checkboxtext}
+                        style={{ opacity: onlyRisk ? 1 : 0.4 }}
+                      >
                         Публикации только с риск-факторами
                       </p>
                     </div>
                     <div className={style.checkboxitem}>
-                      <input type="checkbox" className={style.checkbox} />
-                      <p className={style.checkboxtext}>
+                      <input
+                        type="checkbox"
+                        className={style.checkbox}
+                        checked={technicalNews}
+                        onChange={() => setTechnicalNews}
+                      />
+                      <p
+                        className={style.checkboxtext}
+                        style={{ opacity: technicalNews ? 1 : 0.4 }}
+                      >
                         Включать технические новости рынков
                       </p>
                     </div>
                     <div className={style.checkboxitem}>
-                      <input type="checkbox" className={style.checkbox} />
-                      <p className={style.checkboxtext}>
+                      <input
+                        type="checkbox"
+                        className={style.checkbox}
+                        checked={previews}
+                        onChange={() => setPreviews}
+                      />
+                      <p
+                        className={style.checkboxtext}
+                        style={{ opacity: previews ? 1 : 0.4 }}
+                      >
                         Включать анонсы и календари
                       </p>
                     </div>
                     <div className={style.checkboxitem}>
-                      <input type="checkbox" className={style.checkbox} />
-                      <p className={style.checkboxtext}>
+                      <input
+                        type="checkbox"
+                        className={style.checkbox}
+                        checked={newsBulletin}
+                        onChange={() => setNewsBulletin}
+                      />
+                      <p
+                        className={style.checkboxtext}
+                        style={{ opacity: newsBulletin ? 1 : 0.4 }}
+                      >
                         Включать сводки новостей
                       </p>
                     </div>
@@ -110,21 +222,29 @@ export default function Search() {
                       type="date"
                       name="start"
                       id="start"
+                      value={startDate}
+                      onChange={handleChangeStartDate}
                       className={`${style.input} ${style.dateinput}`}
                       placeholder="Дата начала"
+                      required
                     />
                     <input
                       type="date"
                       name="finish"
                       id="finish"
+                      value={finishtDate}
+                      onChange={handleChangeFinishDate}
                       className={`${style.input} ${style.dateinput}`}
                       placeholder="Дата конца"
+                      required
                     />
                   </div>
                   <div className={style.submitblock}>
                     <button
                       type="submit"
                       className={`${allstyles.button} ${style.submitbutton}`}
+                      disabled={!inn || !countDocs || !startDate || !finishtDate}
+                      style={{opacity: !inn || !countDocs || !startDate || !finishtDate ? 0.5 : 1}}
                     >
                       Поиск
                     </button>
